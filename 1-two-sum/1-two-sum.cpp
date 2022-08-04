9b12b645baf41afd87864a0d8187261aa1e6d457
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-04 13:35:00 +0530

    Time: 67 ms (39.96%), Space: 15.9 MB (5.76%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,15 +5,13 @@ public:
         
         int n = nums.size();
         map<int,vector<int>> ind;
-        map<int,bool> present;
         for ( int i = 0; i<n; i++ ){
-            present[nums[i]] = true;
             ind[nums[i]].push_back(i);
         }
         
         for ( int i = 0; i<n; i++ ){
             int val = target - nums[i];
-            if ( val != nums[i] && present[val] && ind[val].size() > 0 ){
+            if ( val != nums[i] && ind[val].size() > 0 ){
                 return {i,ind[val][0]};
             } else if ( val == nums[i] && ind[val].size() >= 2 ){
                 return {i,ind[val][1]};
