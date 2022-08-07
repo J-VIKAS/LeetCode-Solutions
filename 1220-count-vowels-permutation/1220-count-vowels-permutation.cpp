caebf5bb551a1af7a6cc82b2832e2463b4efbfc6
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-07 09:10:23 +0530

    Time: 3 ms (95.02%), Space: 5.9 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
     
-    long long int rec( int i, char ch, int n, vector<vector<long long int>> &dp){
-        if ( i == n ){
-            return 1;
-        }
-        
-        if ( dp[i][ch-'a'] != -1 ){
-            return dp[i][ch-'a'];
-        }
-        
-        if ( ch == 'a' ){
-            return dp[i][ch-'a'] = (rec(i+1,'e',n,dp))%(1000000007);            
-        } else if ( ch == 'e' ){
-            return dp[i][ch-'a'] = (rec(i+1,'a',n,dp) + rec(i+1,'i',n,dp))%(1000000007);
-        } else if ( ch == 'i' ){
-            return dp[i][ch-'a'] = (rec(i+1,'a',n,dp) + rec(i+1,'e',n,dp) + rec(i+1,'o',n,dp) + rec(i+1,'u',n,dp))%(1000000007);
-        } else if ( ch == 'o' ){
-            return dp[i][ch-'a'] = (rec(i+1,'i',n,dp) + rec(i+1,'u',n,dp))%(1000000007);
-        } else {
-            return dp[i][ch-'a'] = (rec(i+1,'a',n,dp))%(1000000007);        
-        }
-        
-    }
-    
     int countVowelPermutation(int n) {
         
-        vector<vector<long long int>> dp(n,vector<long long int>(26,-1));
-        vector<char> vowels = {'a','e','i','o','u'};
-        int total = 0;
-        for ( int i = 0; i<vowels.size(); i++ ){
-            total += dp[0][vowels[i]-'a'] = rec(1,vowels[i],n,dp);
-            total = total%(1000000007);
+        long a = 1, e = 1, i = 1, o = 1, u = 1, mod = pow(10, 9)+7;
+        long a2, e2, i2, o2, u2; 
+        
+        for (int j = 2; j <= n; j++) {
+            a2 = (e + i + u) % mod;
+            e2 = (a + i) % mod;
+            i2 = (e + o) % mod;
+            o2 = i;
+            u2 = (o + i) % mod;
+            
+            a = a2, e = e2, i = i2, o = o2, u = u2;
         }
         
-        return total;
+        return (a + e + i + o + u) % mod;
     }
 };
