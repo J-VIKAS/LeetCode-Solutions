6fc73525a06d2da71323b019839c3848b25d4875
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-12 23:30:14 +0530

    Time: 383 ms (15.49%), Space: 144.7 MB (17.84%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    bool check( string s, int l, int r ){
+        while ( l <= r ){
+            if ( s[l++] != s[r--] ){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    void rec( int i, string s, vector<string> a, vector<vector<string>> &ans){
+        
+        if ( i == s.size() ){
+            ans.push_back(a);
+            return;
+        }
+        
+        for ( int j = i; j<s.size(); j++ ){
+            if ( check(s,i,j) ){
+                a.push_back(s.substr(i,j-i+1));
+                rec(j+1,s,a,ans);
+                a.pop_back();
+            }
+        }
+        
+    }
+    
+    vector<vector<string>> partition(string s) {
+        
+        vector<string> a;
+        vector<vector<string>> ans;
+        rec(0,s,a,ans);
+        return ans;
+    }
+};
