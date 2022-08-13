bb3bb75a261fa1f1010ba322f3da810a085acdf0
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-13 11:25:02 +0530

    Time: 617 ms (39.52%), Space: 22.7 MB (76.53%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
     
-    bool check( string s, int l, int r, int len, unordered_map<string,bool> &present, unordered_map<string,int> freq  ){
-        
-        string temp = "";
-        int i = l;
-        while ( l <= r ){
-            temp += s[l];
-            l++;
-            if ( (l-i)%len == 0 ){
-                if ( !(present[ temp ] && freq[ temp ] > 0) ){
-                    return false;
-                }
-                freq[ temp ]--;
-                temp = "";
-            }
-        }
-        return true;
-        
-    }
-    
     vector<int> findSubstring(string s, vector<string>& words) {
         
-        int n = s.size(), m =words.size(), len = words[0].size();
+        int len = words[0].length(), noOfWords = words.size();
+        vector<int> result;
+        map<string, int> wordMap;
         
-        unordered_map<string,bool> present;
-        unordered_map<string,int> freq;
-        for ( int i = 0; i<m; i++ ){
-            present[ words[i] ] = true;
-            freq[ words[i] ]++;
+        if (len * noOfWords > s.length())
+            return result;
+        
+        for (string word : words)
+            wordMap[word]++;
+            
+        for (int idx = 0; idx <= s.length() - (noOfWords * len); idx++) {
+            map<string, int> wordsUsedMap;
+            
+            for (int chunkIdx = idx; chunkIdx < idx + (noOfWords * len); chunkIdx += len) {
+                string cur = s.substr(chunkIdx, len);
+                if(wordMap.find(cur) == wordMap.end())
+                    break;
+
+                wordsUsedMap[cur]++;
+                
+                if(wordsUsedMap[cur] > wordMap[cur])
+                    break;
+            }
+            
+            if (wordsUsedMap == wordMap)
+                result.push_back(idx);
         }
         
-        vector<int> ans;
-        for ( int i = 0; i<n-len*m+1; i++ ){
-            string temp = "";
-            for ( int j = i; j<i+len*m; j++ ){
-                temp += s[j];
-            }
-            if ( check(s,i,i+len*m-1,len,present,freq) ){
-                ans.push_back(i);
-            }
-        }
-        
-        return ans;        
+        return result;       
     }
 };
