87828bbb5edfed95a798b607e1dfd9eb490a889f
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-13 11:36:08 +0530

    Time: 1538 ms (18.92%), Space: 299 MB (18.71%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
     
-    bool check( string s, int l, int r, int len, unordered_map<string,int> freq  ){
-        
-        string temp = "";
-        int i = l;
-        while ( l <= r ){
-            temp += s[l];
-            l++;
-            if ( (l-i)%len == 0 ){
-                if ( !( freq[ temp ] > 0) ){
-                    return false;
-                }
-                freq[ temp ]--;
-                temp = "";
-            }
-        }
-        return true;
-        
-    }
-    
     vector<int> findSubstring(string s, vector<string>& words) {
         
-        int n = s.size(), m =words.size(), len = words[0].size();
+        int n = s.size(), m = words.size(), len = words[0].size();
+        unordered_map<string,int> present;
+        vector<int> ans;
         
-        unordered_map<string,int> freq;
         for ( int i = 0; i<m; i++ ){
-            freq[ words[i] ]++;
+            present[ words[i] ]++;
         }
         
-        vector<int> ans;
-        for ( int i = 0; i<n-len*m+1; i++ ){
-            string temp = "";
-            for ( int j = i; j<i+len*m; j++ ){
-                temp += s[j];
+        for ( int i = 0; i<n-(len*m)+1; i++ ){
+            unordered_map<string,int> temp;
+            for ( int j = i; j<i+(len*m); j+=len ){
+                temp[ s.substr(j,len) ]++;                
             }
-            if ( check(s,i,i+len*m-1,len,freq) ){
+            if ( temp == present ){
                 ans.push_back(i);
             }
         }
         
-        return ans;        
+        return ans;
     }
 };
