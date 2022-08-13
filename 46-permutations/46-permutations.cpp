54469e33741f68e10923c9622f87aca57aa2aa5b
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-13 14:50:48 +0530

    Time: 3 ms (82.69%), Space: 7.8 MB (45.39%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    
-    vector<vector<int>> ans;
-    void rec(int n, map<int,bool> &taken, vector<int> nums, vector<int> a){
-        
-        bool check = false;
-        for ( int i = 0; i<n; i++ ){
-            if ( !taken[i] ){
-                check = true;
-                taken[i] = true;
-                a.push_back(nums[i]);
-                rec(n,taken,nums,a);
-                a.pop_back();
-                taken[i] = false;
-            }
+    void findpermutations(vector<int>& nums,vector<vector<int>>&result, vector<int>&temp,int vis[]){
+        if(temp.size() == nums.size()){
+            result.push_back(temp);
         }
-        
-        if ( !check ){
-            ans.push_back(a);
-            return;
+        for(int i=0;i<nums.size();i++){
+             if(vis[i]!=1){
+                temp.push_back(nums[i]);
+                vis[i] = 1;
+                findpermutations(nums,result,temp,vis);
+                vis[i] = 0;
+                temp.pop_back();
+            }   
         }
-        
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
         
-        map<int,bool> taken;
-        vector<int> a;
-        rec(nums.size(),taken,nums,a);
-        return ans;
-        
+        vector<vector<int>> result;
+        int index = 0;
+        int n = nums.size();
+        int vis[n];
+        for(int i=0;i<n;i++){
+            vis[i] == 0;
+        }
+        vector<int> temp;
+        findpermutations(nums,result,temp, vis);
+        return result;
     }
 };
