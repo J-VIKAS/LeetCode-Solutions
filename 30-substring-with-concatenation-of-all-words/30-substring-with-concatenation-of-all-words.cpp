35b758cb1d19f33926998e011a5fe4c90e9ed534
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-13 11:26:11 +0530

    Time: 2368 ms (5.04%), Space: 584 MB (5.02%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     
+    bool check( string s, int l, int r, int len, unordered_map<string,int> freq  ){
+        
+        string temp = "";
+        int i = l;
+        while ( l <= r ){
+            temp += s[l];
+            l++;
+            if ( (l-i)%len == 0 ){
+                if ( !( freq[ temp ] > 0) ){
+                    return false;
+                }
+                freq[ temp ]--;
+                temp = "";
+            }
+        }
+        return true;
+        
+    }
+    
     vector<int> findSubstring(string s, vector<string>& words) {
         
-        int len = words[0].length(), noOfWords = words.size();
-        vector<int> result;
-        map<string, int> wordMap;
+        int n = s.size(), m =words.size(), len = words[0].size();
         
-        if (len * noOfWords > s.length())
-            return result;
-        
-        for (string word : words)
-            wordMap[word]++;
-            
-        for (int idx = 0; idx <= s.length() - (noOfWords * len); idx++) {
-            map<string, int> wordsUsedMap;
-            
-            for (int chunkIdx = idx; chunkIdx < idx + (noOfWords * len); chunkIdx += len) {
-                string cur = s.substr(chunkIdx, len);
-                if(wordMap.find(cur) == wordMap.end())
-                    break;
-
-                wordsUsedMap[cur]++;
-                
-                if(wordsUsedMap[cur] > wordMap[cur])
-                    break;
-            }
-            
-            if (wordsUsedMap == wordMap)
-                result.push_back(idx);
+        unordered_map<string,int> freq;
+        for ( int i = 0; i<m; i++ ){
+            freq[ words[i] ]++;
         }
         
-        return result;       
+        vector<int> ans;
+        for ( int i = 0; i<n-len*m+1; i++ ){
+            string temp = "";
+            for ( int j = i; j<i+len*m; j++ ){
+                temp += s[j];
+            }
+            if ( check(s,i,i+len*m-1,len,freq) ){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;        
     }
 };
