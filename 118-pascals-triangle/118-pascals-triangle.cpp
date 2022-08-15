92794c4de5e2c7c0a4d1251f9c54de623ea8e143
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-15 17:05:10 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.27%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -7,11 +7,9 @@ public:
         if ( numRows == 1 ) return pascals;
         pascals.push_back({1,1});
         for ( int i = 2; i<numRows; i++ ){
-            vector<int> p = {1};
-            for ( int j = 1; j<i; j++ ){
-                p.push_back(pascals[i-1][j-1]+pascals[i-1][j]);
-            }
-            p.push_back(1);
+            vector<int> p(i+1,1);
+            for ( int j = 1; j<i; j++ )
+                p[j] = pascals[i-1][j-1]+pascals[i-1][j];
             pascals.push_back(p);
         }
         return pascals;        
