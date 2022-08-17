f7a0c9e476487fd69e3c574892fb4f9344d27149
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-17 12:15:48 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (13.55%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if ( x < 0 )
-        {
-            x *= -1;
-            if ( n%2 == 0 ){
-                return pow(10,n*log10(x));
-            } else {
-                return (-1)*pow(10,n*log10(x));
-            }
+        if ( x < 0 ){
+            return pow(-1,n)*pow(10,n*log10((x*-1)));
         } else {
             return pow(10,n*log10(x));
         }
