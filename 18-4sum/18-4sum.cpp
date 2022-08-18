f2c8acc767273831aea5e63dfb6d45f651ecd931
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-18 14:46:44 +0530

    Time: 592 ms (12.47%), Space: 52.8 MB (9.62%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -4,17 +4,22 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
         vector<vector<int>> ans;
+        int len = 0;
         map<vector<int>,bool> p;
         sort(nums.begin(),nums.end());
         for ( int i = 0; i<nums.size(); i++ ){
             for ( int j = i+1; j<nums.size()-1; j++ ){
                 long long int t = (long long int)target - nums[i] - nums[j];
                 int l = j+1, r = nums.size()-1;
+                if ( p[{nums[i],nums[j]}] )
+                    continue;
                 while ( l < r ){
                     if ( nums[l] + nums[r] == t ){
-                        if ( !p[{nums[i],nums[j],nums[l],nums[r]}] )
-                            ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        p[{nums[i],nums[j],nums[l],nums[r]}] = true;
+                        vector<int> temp = {nums[i],nums[j],nums[l],nums[r]};
+                        if ( len == 0 || (len != 0 && ans[len-1] != temp ) ){
+                            ans.push_back(temp);
+                            len += 1;
+                        }
                         l += 1;
                     } else if ( nums[l] + nums[r] < t ){
                         l += 1;
@@ -22,6 +27,7 @@ public:
                         r -= 1;
                     }
                 }
+                p[{nums[i],nums[j]}] = true;
             }
         }
         
