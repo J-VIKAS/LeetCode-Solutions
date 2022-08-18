a2524961f9de0c1cb7ea84fe9823782be8e9f60b
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-18 14:39:05 +0530

    Time: 1667 ms (5.02%), Space: 144 MB (5.07%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     
-    void binary_search(vector<int>& nums, int i, int j, long long target, int a, int b, vector<vector<int>>& ans){
-        map<vector<int>,bool> p;
-        while ( i < j ){
-            if ( nums[i] + nums[j] == target && !p[{nums[i],nums[j]}] ){
-                ans.push_back({a,b,nums[i],nums[j]});
-                p[{nums[i],nums[j]}] = true;
-                i += 1;
-            } else if ( nums[i] + nums[j] < target ){
-                i += 1;
-            } else {
-                j -= 1;
-            }
-        } 
-    }
-    
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
-        sort(nums.begin(),nums.end());
-        map<vector<int>,bool> p;
         vector<vector<int>> ans;
+        map<vector<int>,bool> p;
+        sort(nums.begin(),nums.end());
         for ( int i = 0; i<nums.size(); i++ ){
-            for ( int j = i+1; j<nums.size(); j++ ){
-                if ( !p[{nums[i],nums[j]}] )
-                    try { 
-                        binary_search(nums,j+1,nums.size()-1,((long long)target-nums[i]-nums[j]),nums[i],nums[j],ans);
-                    } catch (int k){
-                        continue;
+            for ( int j = i+1; j<nums.size()-1; j++ ){
+                long long int t = (long long int)target - nums[i] - nums[j];
+                int l = j+1, r = nums.size()-1;
+                while ( l < r ){
+                    if ( nums[l] + nums[r] == t ){
+                        if ( !p[{nums[i],nums[j],nums[l],nums[r]}] )
+                            ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        p[{nums[i],nums[j],nums[l],nums[r]}] = true;
+                        l += 1;
+                    } else if ( nums[l] + nums[r] < t ){
+                        l += 1;
+                    } else {
+                        r -= 1;
                     }
-                p[{nums[i],nums[j]}] = true;
+                }
             }
         }
-        return ans;
         
+        return ans;
     }
 };
