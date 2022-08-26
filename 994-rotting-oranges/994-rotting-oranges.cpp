85e1caf4efcd44eee9268d3d325e615255e97d8b
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-26 17:03:07 +0530

    Time: 7 ms (86.02%), Space: 13.5 MB (16.79%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -4,63 +4,44 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         
         int n = grid.size(), m = grid[0].size();
-        map<pair<int,int>,bool> vis;
+        vector<vector<bool>> vis(n, vector<bool>(m,false));
         queue<pair<int,int>> q;
         for ( int i = 0; i<n; i++ ){
             for ( int j = 0; j<m; j++ ){
                 if ( grid[i][j] == 2 ){
                     q.push({i,j});
-                    vis[{i,j}] = true;
+                    vis[i][j] = true;
                 }
             }
         }
-            
+        
+        vector<vector<int>> d = { {-1,0}, {1,0}, {0,-1}, {0,1} };
+        
         int time = -1;
-        if ( q.empty() ){
-            time = 0;
-        }
+        if ( q.empty() ) time = 0;
         while ( !q.empty() ){
             time += 1;
             int k = q.size();
-                
             while ( k-- ){
                 pair<int,int> p = q.front();
                 q.pop();
-                    
-                if ( p.first-1 >= 0 && grid[p.first-1][p.second] == 1 && !vis[{p.first-1,p.second}] ){
-                    grid[p.first-1][p.second] = 2;
-                    vis[{p.first-1,p.second}] = true;
-                    q.push({p.first-1,p.second});
-                }
-                    
-                if ( p.first+1 < n && grid[p.first+1][p.second] == 1 && !vis[{p.first+1,p.second}] ){
-                    grid[p.first+1][p.second] = 2;
-                    vis[{p.first+1,p.second}] = true;
-                    q.push({p.first+1,p.second});
-                }
-                    
-                if ( p.second-1 >= 0 && grid[p.first][p.second-1] == 1 && !vis[{p.first,p.second-1}] ){
-                    grid[p.first][p.second-1] = 2;
-                    vis[{p.first,p.second-1}] = true;
-                    q.push({p.first,p.second-1});
-                }
-                    
-                if ( p.second+1 < m && grid[p.first][p.second+1] == 1 && !vis[{p.first,p.second+1}] ){
-                    grid[p.first][p.second+1] = 2;
-                    vis[{p.first,p.second+1}] = true;
-                    q.push({p.first,p.second+1});
+                for ( int i = 0; i<d.size(); i++ ){
+                    int a = p.first + d[i][0], b = p.second + d[i][1];
+                    if ( a >= 0 && a < n && b >= 0 && b < m && grid[a][b] == 1 && !vis[a][b] ){
+                        vis[a][b] = true;
+                        grid[a][b] = 2;
+                        q.push({a,b});
+                    }
                 }
             }
         }
         
         for ( int i = 0; i<n; i++ ){
             for ( int j = 0; j<m; j++ ){
-                if ( grid[i][j] == 1 ){
-                    return -1;
-                }
+                if ( grid[i][j] == 1 ) return -1;
             }
         }
         
-        return time;        
+        return time;
     }
 };
