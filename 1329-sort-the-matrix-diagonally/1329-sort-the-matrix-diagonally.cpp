0ed8529243a8af7725ee34fc35a9e655f7fd4e22
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-28 17:27:01 +0530

    Time: 77 ms (5.15%), Space: 18 MB (5.01%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -5,28 +5,16 @@ public:
         vector<int> mer;
         int fi = si, fj = sj, sei = mi+1, sej = mj+1;
         while ( fi <= mi && fj <= mj && sei <= ei && sej <= ej ){
-            if ( mat[fi][fj] < mat[sei][sej] ){
-                mer.push_back(mat[fi++][fj++]);
-            } else {
-                mer.push_back(mat[sei++][sej++]);
-            }
+            if ( mat[fi][fj] < mat[sei][sej] )  mer.push_back(mat[fi++][fj++]);
+            else  mer.push_back(mat[sei++][sej++]);
         }
-        while ( fi <= mi && fj <= mj ){
-            mer.push_back(mat[fi++][fj++]);
-        }
-        while ( sei <= ei && sej <= ej ){
-            mer.push_back(mat[sei++][sej++]);
-        }
-        
-        for ( int i = 0; i<mer.size(); i++ ){
-            mat[si++][sj++] = mer[i];
-        }
-        
+        while ( fi <= mi && fj <= mj )  mer.push_back(mat[fi++][fj++]);
+        while ( sei <= ei && sej <= ej )    mer.push_back(mat[sei++][sej++]);
+        for ( int i = 0; i<mer.size(); i++ )    mat[si++][sj++] = mer[i];
     }
     
     void merge_sort( int si, int sj, int ei, int ej, vector<vector<int>> &mat ){
-        if ( si >= ei && sj >= ej )
-            return;
+        if ( si >= ei && sj >= ej ) return;
         int mi = si + (ei-si)/2, mj = sj + (ej-sj)/2;
         merge_sort(si,sj,mi,mj,mat);
         merge_sort(mi+1,mj+1,ei,ej,mat);
@@ -34,25 +22,17 @@ public:
     }
     
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        
         int n = mat.size(), m = mat[0].size();
         for ( int i = n-1; i>=0; i-- ){
             int ei = i, ej = 0;
-            while ( ei < n && ej < m ){
-                ei++;
-                ej++;
-            }
+            while ( ei < n && ej < m ){ ei++;ej++; }
             merge_sort(i,0,ei-1,ej-1,mat);
         }
         for ( int j = 0; j<m; j++ ){
             int ei = 0, ej = j;
-            while ( ei < n && ej < m ){
-                ei++;
-                ej++;
-            }
+            while ( ei < n && ej < m ){ ei++;ej++; }
             merge_sort(0,j,ei-1,ej-1,mat);
         }
         return mat;
-
     }
 };
