2de92984055adfd87a5cf56b04eb197c660709e9
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-08-29 14:39:40 +0530

    Time: 4 ms (32.19%), Space: 6.3 MB (60.73%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    vector<int> computeLPS(string &p, int m){
+        int len = 0, i = 1;
+        vector<int> lps(m,0);
+        while ( i < m ){
+            if ( p[i] == p[len] ){
+                lps[i] = len + 1;
+                len++; i++;
+            } else {
+                if ( len != 0 ){
+                    len = lps[len-1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    int KMPSearch(string &s, string &p){
+        s += "1111111111112312312312312312312312321";
+        int n = s.size(), m = p.size(), i = 0, j = 0;
+        vector<int> lps = computeLPS(p,m);
+        while ( i < n-m+1 ){
+            if ( s[i] == p[j] ){
+                i++; j++;
+            } else {
+                if ( j != 0 ){
+                    j = lps[j-1];
+                } else {
+                    i++;
+                }
+            }
+            if ( j == m ){
+                return i-j;
+            }
+        }
+        return -1;
+    }
+    
+    int strStr(string haystack, string needle) {
+        return KMPSearch(haystack,needle);
+    }
+};
