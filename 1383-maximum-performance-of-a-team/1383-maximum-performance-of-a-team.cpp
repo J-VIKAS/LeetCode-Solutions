d9277a754c1ba9a44289d5af316e1357a87e6262
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-11 17:53:26 +0530

    Time: 191 ms (20.60%), Space: 36.9 MB (56.16%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    static bool cmp( pair<int,int> a, pair<int,int> b ){
+        return a.first > b.first;
+    }
+    
+    int maxPerformance( int n, vector<int>& speed, vector<int>& efficiency, int k ) {
+        
+        vector<pair<int,int>> v;
+        for ( int i = 0; i<n; i++ ){
+            v.push_back({efficiency[i],speed[i]});
+        }
+        sort(v.begin(),v.end(),cmp);
+        
+        priority_queue<int,vector<int>,greater<int>> q;
+        long ans = 0, total_sum = 0;
+        
+        for ( int i = 0; i<n; i++ ){
+            q.push(v[i].second);
+            total_sum += v[i].second;
+            if ( q.size() > k ){
+                total_sum -= q.top();
+                q.pop();
+            }
+            ans = max(ans,v[i].first*total_sum);
+        }
+        
+        return ans%(1000000000+7);
+    }
+};
