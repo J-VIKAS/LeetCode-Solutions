9d705d9d9f2b034b4256df1e2935cde2de65c95d
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-13 12:01:30 +0530

    Time: 23 ms (41.65%), Space: 14 MB (38.65%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,58 +1,18 @@
 class Solution {
 public:
-    
-    string convert_to_binary( int data ){
-        string ans = "";
-        stack<char> st;
-        while ( data ){
-            if ( data%2 == 0 )  st.push('0');
-            else    st.push('1');
-            data /= 2;
-        }
-        
-        int k = 8-st.size();
-        while ( k-- ){
-            st.push('0');
-        }
-        
-        while ( !st.empty() ){
-            ans += st.top();
-            st.pop();
-        }
-        return ans;       
-    }
-    
-    int ones( string &s ){
-        int count = 0;
-        for ( int i = 0; i<s.size(); i++ ){
-            if ( s[i] == '0' ){
-                break;
-            }
-            count += 1;
-        }
-        return count;
-    }
 
     bool validUtf8(vector<int>& data) {
         
-        int n = data.size(), req = 0;
-        for ( int i = 0; i<n; i++ ){
-            string bin = convert_to_binary(data[i]);
-            int total_ones = ones(bin);
-            if ( total_ones > 4 ) return false;
-            else if ( req == 0 && total_ones == 1 ){
-                return false;
-            } else if ( req == 0 && total_ones > 0 ){
-                req = total_ones-1;
-            } else if ( req > 0 && total_ones != 1 ){
-                return false;
-            } else if ( req > 0 && total_ones == 1 ) {
-                req -= 1;
-            }
+        int total = 0;
+        for ( int i = 0; i<data.size(); i++ ){
+            if ( data[i] >> 3 == 31  ) return false;
+            else if ( data[i] >> 4 == 15 ) total += 3;
+            else if ( data[i] >> 5 == 7 ) total += 2;
+            else if ( data[i] >> 6 == 3 ) total += 1;
+            else if ( data[i] >> 7 == 1 ) total -= 1;
+            else total += 0;
         }
-        
-        if ( req == 0 ) return true; 
-        return false;
-    
+        return total == 0;
+            
     }
 };
