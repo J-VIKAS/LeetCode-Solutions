c7b067652ab0c1f39c9f5fb734e0a9ac5023060b
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-15 11:28:13 +0530

    Time: 560 ms (74.59%), Space: 119.1 MB (96.04%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -3,18 +3,20 @@ public:
     vector<int> findOriginalArray(vector<int>& changed) {
         int n = changed.size();
         if ( n%2 != 0 ) return {};
+        sort(changed.begin(),changed.end());
         vector<int> ans;
-        sort(changed.begin(), changed.end());
-        int p[400005] = {0};
+        int p[200003] = {0};
         for ( int i = 0; i<n; i++ ){
             p[ changed[i] ]++;
         }
         for ( int i = 0; i<n; i++ ){
-            if ( p[ changed[i] ] > 0 ){ 
+            if ( p[ changed[i] ] > 0 ){
                 p[ changed[i] ]--;
                 if ( p[ changed[i]*2 ] > 0 ){
                     ans.push_back( changed[i] );
                     p [ changed[i]*2 ]--;
+                } else {
+                    p[ changed[i] ]++;
                 }
             }
         }
