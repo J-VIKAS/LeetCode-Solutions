6f753e3a1185e937f9738c3a72ce74710e22a90a
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-16 18:56:56 +0530

    Time: 27 ms (71.39%), Space: 26.4 MB (28.32%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    map<int,int> ind;
+    
+    TreeNode* rec( TreeNode* root, int si, int se, vector<int> &inorder, int pi, int pe, vector<int> &preorder ){
+        
+        if ( se-si < 0 || pe-pi < 0 )   return NULL;
+        root = new TreeNode(preorder[pi]);
+        int k = ind[preorder[pi]] - si;
+        
+//         cout << "k : " << k << "\n";
+        
+//         cout << "inserted root " << preorder[pi] << "\n";
+        
+//         cout << "left :\n";
+//         cout << "inorder : indexes (" << si << "," << ind[preorder[pi]]-1 << ");\n";
+//         cout << "preorder : indexes (" << pi+1 << "," << pi+k+1 << ");\n";
+        
+        root->left = rec(root->left,si,ind[preorder[pi]]-1,inorder,pi+1,pi+k+1,preorder);
+        
+        // cout << "right :\n";
+        // cout << "inorder : indexes (" << ind[preorder[pi]]+1 << "," << se << ");\n";
+        // cout << "preorder : indexes (" << pi+k+1 << "," << pe << ");\n";
+        
+        root->right = rec(root->right,ind[preorder[pi]]+1,se,inorder,pi+k+1,pe,preorder);
+        
+        return root;
+        
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+        int n = inorder.size();
+        for ( int i = 0; i<n; i++ ) ind[inorder[i]] = i;
+        TreeNode* root = rec( root, 0, n-1, inorder, 0, n-1, preorder );
+        return root;
+    }
+};
