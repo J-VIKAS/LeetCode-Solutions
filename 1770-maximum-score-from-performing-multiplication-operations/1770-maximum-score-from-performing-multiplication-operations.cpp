4d5576816e5bd0b2e6c89c55af96741b4d49ae1d
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-16 12:23:02 +0530

    Time: 792 ms (34.85%), Space: 120.1 MB (66.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     
-    double dp[1001][1001];
-    int n,m;
-    double rec( int j, int s, int e,vector<int> &nums, vector<int> &multipliers ){
+    int rec( int j, int s, int e, vector<int> &nums, vector<int> &multipliers, vector<vector<int>> &dp ){
         
-        if ( s > e || j == m ) return 0;
+        if ( s > e || j == multipliers.size() ) return 0;
          
-           
-        if ( dp[s][j] != -1.1 ) return dp[s][j];
+        if ( dp[s][j] != INT_MIN ) return dp[s][j];
         
-        return dp[s][j] = max( multipliers[j]*nums[s] + rec(j+1,s+1,e,nums,multipliers),
-                    multipliers[j]*nums[e] + rec(j+1,s,e-1,nums,multipliers) );
+        return dp[s][j] = max( multipliers[j]*nums[s] + rec(j+1,s+1,e,nums,multipliers,dp),
+                    multipliers[j]*nums[e] + rec(j+1,s,e-1,nums,multipliers,dp) );
         
     }
     
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         
-        n = nums.size(), m = multipliers.size();
-         
-        for( int i = 0; i<1001; i++ ){
-           for ( int j = 0; j<1001; j++ ){
-               dp[i][j] = -1.1;
-           }
-        }
-        
-        int x = (rec(0,0,n-1,nums,multipliers));
-        return x;
+        int n = nums.size(), m = multipliers.size();
+        vector<vector<int>> dp(m+1,vector<int>(m+1,INT_MIN));
+        return  rec(0,0,n-1,nums,multipliers,dp); 
         
     }
 };
