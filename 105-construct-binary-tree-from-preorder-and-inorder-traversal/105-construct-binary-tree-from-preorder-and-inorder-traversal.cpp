8f153a3b6f193d001e459bc84051a8adc4df5d89
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-16 18:57:47 +0530

    Time: 30 ms (64.76%), Space: 26.2 MB (59.08%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -15,33 +15,15 @@ public:
     map<int,int> ind;
     
     TreeNode* rec( TreeNode* root, int si, int se, vector<int> &inorder, int pi, int pe, vector<int> &preorder ){
-        
         if ( se-si < 0 || pe-pi < 0 )   return NULL;
         root = new TreeNode(preorder[pi]);
-        int k = ind[preorder[pi]] - si;
-        
-//         cout << "k : " << k << "\n";
-        
-//         cout << "inserted root " << preorder[pi] << "\n";
-        
-//         cout << "left :\n";
-//         cout << "inorder : indexes (" << si << "," << ind[preorder[pi]]-1 << ");\n";
-//         cout << "preorder : indexes (" << pi+1 << "," << pi+k+1 << ");\n";
-        
-        root->left = rec(root->left,si,ind[preorder[pi]]-1,inorder,pi+1,pi+k+1,preorder);
-        
-        // cout << "right :\n";
-        // cout << "inorder : indexes (" << ind[preorder[pi]]+1 << "," << se << ");\n";
-        // cout << "preorder : indexes (" << pi+k+1 << "," << pe << ");\n";
-        
+        int k = ind[preorder[pi]] - si;        
+        root->left = rec(root->left,si,ind[preorder[pi]]-1,inorder,pi+1,pi+k+1,preorder);        
         root->right = rec(root->right,ind[preorder[pi]]+1,se,inorder,pi+k+1,pe,preorder);
-        
         return root;
-        
     }
     
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
         int n = inorder.size();
         for ( int i = 0; i<n; i++ ) ind[inorder[i]] = i;
         TreeNode* root = rec( root, 0, n-1, inorder, 0, n-1, preorder );
