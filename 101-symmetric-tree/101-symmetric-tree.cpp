60b9866b9ea1fe956ba07d4295950766d1046bd4
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-16 15:13:31 +0530

    Time: 3 ms (94.41%), Space: 16.5 MB (17.65%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -12,60 +12,24 @@
 class Solution {
 public:
     
-    void bfs1( TreeNode* root, vector<int> &a ){
-        queue<TreeNode*> q;
-        q.push(root);
-        while ( !q.empty() ){
-            int k = q.size();
-            bool check = false;
-            while ( k-- ){
-                TreeNode* p = q.front();
-                q.pop();
-                if (p) a.push_back(p->val);
-                else a.push_back(-101);
-                if ( p != NULL && ( p->left || p->right ) ) check = true;
-                if (p != NULL && p->left) q.push(p->left);
-                else q.push(NULL);
-                if (p != NULL && p->right) q.push(p->right);
-                else q.push(NULL);
-            }
-            if ( !check ) break;
-        }
-    }
+    bool check = true;
     
-    void bfs2( TreeNode* root, vector<int> &a ){
-        queue<TreeNode*> q;
-        q.push(root);
-        while ( !q.empty() ){
-            int k = q.size();
-            bool check = false;
-            while ( k-- ){
-                TreeNode* p = q.front();
-                q.pop();
-                if (p) a.push_back(p->val);
-                else a.push_back(-101);
-                if ( p != NULL && ( p->left || p->right ) ) check = true;
-                if (p != NULL && p->right) q.push(p->right);
-                else q.push(NULL);
-                if (p != NULL && p->left) q.push(p->left);
-                else q.push(NULL);
+    void rec( TreeNode* l, TreeNode* r ){
+        if ( l == NULL && r == NULL ) return;
+        else if ( l != NULL && r != NULL ){
+            if ( l->val != r->val ){
+                check = false;
+                return;
             }
-            if ( !check ) break;
+            rec( l->right, r->left );
+            rec( l->left, r->right );
+        } else {
+            check = false;
         }
     }
     
     bool isSymmetric(TreeNode* root) {
-        vector<int> l, r;
-        bfs1(root->left,l);
-        bfs2(root->right,r);
-        // cout << "l : ";
-        // for ( int i = 0; i<l.size(); i++ ){
-        //     cout << l[i] << " ";
-        // } cout << "\n";
-        // cout << "r : ";
-        // for ( int i = 0; i<r.size(); i++ ){
-        //     cout << r[i] << " ";
-        // } cout << "\n";
-        return l == r; 
+        rec(root->left,root->right);
+        return check;
     }
 };
