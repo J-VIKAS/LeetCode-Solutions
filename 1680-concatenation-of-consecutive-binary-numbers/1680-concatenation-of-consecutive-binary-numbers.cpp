7a56359a0fd743f23ac39ae873a617f3f466be4a
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-23 23:59:03 +0530

    Time: 246 ms (38.04%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -4,13 +4,10 @@ public:
     long long MOD = 1000000000 + 7;
          
     int concatenatedBinary(int n) {
-        
+               
         long long ans = 0;
         for ( long long i = 1; i<=n; i++ ){
-            ans <<= ((int)log2(i)+1);
-            ans %= MOD;
-            ans += i;
-            ans %= MOD;
+            ans = ((ans<<(int)log2(i)+1)%MOD + i)%MOD;
         }
         return (int)ans%MOD;
         
