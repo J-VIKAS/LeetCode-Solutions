74be44dd0b32d3375c0e423a4b0111bcc7f7f794
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-27 16:05:37 +0530

    Time: 258 ms (72.55%), Space: 19.4 MB (80.98%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -8,25 +8,20 @@ public:
     }
     
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        
         int n = scores.size();
         vector<pair<int,int>> vec;
         int l[1001] = {0};
         for ( int i = 0; i<n; i++ ) vec.push_back({ages[i],scores[i]});
         sort(vec.begin(),vec.end(),cmp);
         for ( int i = 0; i<n; i++ ) l[i] = vec[i].second;
-        
         int ans = INT_MIN;
         for ( int i = 0; i<n; i++ ){
             int k = 0;
             for ( int j = i-1; j>=0; j-- ){
-                if ( vec[i].second >= vec[j].second ){
-                    k = max(k,l[j]);
-                }
+                if ( vec[i].second >= vec[j].second )   k = max(k,l[j]);
             }
-            ans = max(ans,l[i]+=k);
+            ans = max(ans,l[i] += k);
         }
-        
         return ans;
     }
 };
