0ba758d628e21a663f7ed417f265402c2e3c5c6b
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-28 16:02:52 +0530

    Time: 649 ms (5.00%), Space: 7.6 MB (72.51%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -10,9 +10,7 @@ public:
         else if ( i == n ) return m-j;
         if ( dp[i][j] != -1 )   return dp[i][j];
         if ( word1[i] == word2[j] ) return dp[i][j] = rec(i+1,j+1,word1,word2);
-        return dp[i][j] = 1 + min( { rec(i+1,j,word1,word2), rec(i,j+1,word1,word2),
-                      rec(i,j+1,word1,word2), rec(i+1,j,word1,word2),
-                      rec(i+1,j+1,word1,word2) } );
+        return dp[i][j] = 1 + min( { rec(i+1,j,word1,word2), rec(i,j+1,word1,word2), rec(i+1,j+1,word1,word2) } );
     }
     
     int minDistance(string word1, string word2) {
@@ -20,7 +18,7 @@ public:
         for ( int i = 0; i<501; i++ ){
             for ( int j = 0; j<501; j++ )   dp[i][j] = -1;
         }
-        
+
         n = word1.size(), m = word2.size();
         return rec(0,0,word1,word2);
     }
