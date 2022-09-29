2913fbda2c14ad65007511ab9f1b09fc5c008174
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-29 09:23:04 +0530

    Time: 114 ms (48.27%), Space: 31.9 MB (52.53%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -17,12 +17,8 @@ public:
             if ( abs(arr[l]-x) <= abs(arr[r]-x) )  ans.push_back(arr[l--]);
             else ans.push_back(arr[r++]);
         }
-        while ( l >= 0 && r-l-1 < k ){
-            ans.push_back(arr[l--]);
-        }
-        while ( r < n && r-l-1 < k ){
-            ans.push_back(arr[r++]);
-        }
+        while ( l >= 0 && r-l-1 < k )   ans.push_back(arr[l--]);
+        while ( r < n && r-l-1 < k )    ans.push_back(arr[r++]);
         sort(ans.begin(),ans.end());
         return ans;
         
