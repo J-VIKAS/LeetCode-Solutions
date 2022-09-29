9ad614ff27a382c0a499ff807dfc43330d14f3b0
Jagat Vikas Dhayal <51940996+J-VIKAS@users.noreply.github.com>
2022-09-29 23:42:46 +0530

    Time: 139 ms (33.89%), Space: 32.4 MB (51.31%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -12,14 +12,22 @@ public:
         }
         
         vector<int> ans;
+        deque<int> q;
         int l = ind, r = ind+1;
         while ( l >= 0 && r < n && r-l-1 < k ){
-            if ( abs(arr[l]-x) <= abs(arr[r]-x) )  ans.push_back(arr[l--]);
-            else ans.push_back(arr[r++]);
+            if ( abs(arr[l]-x) <= abs(arr[r]-x) )  q.push_front(arr[l--]);
+            else q.push_back(arr[r++]);
         }
-        while ( l >= 0 && r-l-1 < k )   ans.push_back(arr[l--]);
-        while ( r < n && r-l-1 < k )    ans.push_back(arr[r++]);
-        sort(ans.begin(),ans.end());
+        while ( l >= 0 && r-l-1 < k ){
+            q.push_front(arr[l--]);
+        }
+        while ( r < n && r-l-1 < k ){
+            q.push_back(arr[r++]);
+        }
+        while ( !q.empty() ){
+            ans.push_back(q.front());
+            q.pop_front();
+        }
         return ans;
         
     }
